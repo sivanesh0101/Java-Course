38383756d47c383b00ae55da8915fc968c33d0e7
sivanesh0101 <sivaneshprog0101@gmail.com>
2025-11-03 12:03:26 +0530

    new

---
+++ java_basic/codes/DemoEnhancedFor.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class DemoEnhancedFor {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A"); list.add("B"); list.add("C");
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
